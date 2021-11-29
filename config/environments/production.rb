5180d1ac55301b8d5359b5e5cf4e88bbcc3cfa9e
msy7822-ux <masahiro.fukatsu@theport.jp>
2021-11-29 19:58:57 +0900

    本番環境用の設定ファイルを記述

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.cache_store = :redis_cache_store, {
+    url: ENV['REDIS_URL'],
+    expires_in: 90.minutes
+  }
+  config.session_store :redis_store, servers: ENV['REDIS_URL'], expire_after: 1.year
 end
